95aec65402748a87bcdb98fe4deeaa62a89a1fae
baguz28 <bagussyahdewa28@gmail.com>
2021-09-22 19:22:59 +0700

    membuat consturctor

---
+++ constructor.php
@@ -0,0 +1,29 @@
+<?php
+class Produk{
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kisimoto", "Shōnen Jump", 40000);
+$produk2 = new Produk("Arena of Valor", "Tencent Games", "Garena", 30000);
+$produk3 = new Produk("GTA V");
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
+?>
