83c1dac594260965efe0fb6c798cf5e886c9e4b8
baguz28 <bagussyahdewa28@gmail.com>
2021-09-29 14:41:59 +0700

    mecoba constant dan magic function

---
+++ pertemuan 3/constant.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+define('NAMA', 'tito');
+echo NAMA;
+echo "<br>";
+const UMUR = 20;
+echo UMUR; */
+
+/*
+class Coba {
+    const NAMA = 'Tito';
+}
+echo Coba::NAMA; */
+
+// echo __LINE__;
+
+/*
+function coba() {
+    return __FUNCTION__;
+}
+echo coba(); */
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
